098a2b15a86e59818edb366934a97df553e2cd9e
Dr.Kenobi27 <josgarber6@alum.us.es>
2023-11-16 14:15:17 +0100

    feat: User login, register and change pass done #1
    We can login, logout, register and change the password for a user.

--- ardueshop/ardueshop/settings.py
+++ ardueshop/ardueshop/settings.py
@@ -43,6 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'authentication'
     "bootstrap5",
     "base",
     'authentication'
