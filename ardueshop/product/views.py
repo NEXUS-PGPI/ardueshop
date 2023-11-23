cf9a2d647154a0af9279e22fc0574681d4816139
Francisco Benitez <frabenrui1@alum.us.es>
2023-11-23 13:47:56 +0100

    fix: Improve styles
    Styles for catalogue and products have been improved

--- ardueshop/product/views.py
+++ ardueshop/product/views.py
@@ -5,7 +5,9 @@ from .models import Product, Category
 # Create your views here.
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
-    return render(request, 'product/detail.html', {'product': product})
+    quantity_range = range(1, 11)  # This generates numbers from 1 to 10
+
+    return render(request, 'product/detail.html', {'product': product, 'quantity_range': quantity_range})
 def catalogue(request):
     products = Product.objects.all()
     categories = Category.objects.all()
