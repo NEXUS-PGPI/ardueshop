eed420d401f310a1e9c7903ce76155d454ad653e
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-11-28 18:54:25 +0100

    feat: Redesign catalogue

--- ardueshop/cart/views.py
+++ ardueshop/cart/views.py
@@ -13,10 +13,10 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product,
-                 quantity=cd['quantity'],
-                 override_quantity=cd['override'])
-    return redirect('cart:cart_detail')
+        cart.add(
+            product=product, quantity=cd["quantity"], override_quantity=cd["override"]
+        )
+    return redirect("catalogue:catalogue")
 
 
 @require_POST
@@ -24,13 +24,13 @@ def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:cart_detail')
+    return redirect("cart:cart_detail")
 
 
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={
-                            'quantity': item['quantity'],
-                            'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={"quantity": item["quantity"], "override": True}
+        )
+    return render(request, "cart/detail.html", {"cart": cart})
