12f251a8e783a271dd207ed0a1aaeb53fdccad10
Francisco Benitez <frabenrui1@alum.us.es>
2023-12-08 18:52:33 +0100

    fix: Fix vulnerability

--- ardueshop/base/views.py
+++ ardueshop/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from product.models import Category, Product
 from order.models import Order, OrderItem
 from datetime import timedelta
@@ -18,57 +18,60 @@ def order_not_found(request):
 
 
 def sales_report(request):
-    # Get the current date and time in the current time zone
-    current_date = timezone.now()
+    user = request.user
+    if user.is_staff:
+        current_date = timezone.now()
 
-    # Calculate the start date for the last 30 days
-    last_30_days_start = current_date - timedelta(days=30)
+        # Calculate the start date for the last 30 days
+        last_30_days_start = current_date - timedelta(days=30)
 
-    # Query to get the number of orders from the last 30 days
-    orders_last_30_days = Order.objects.filter(
-        created__range=(last_30_days_start, current_date)
-    )
+        # Query to get the number of orders from the last 30 days
+        orders_last_30_days = Order.objects.filter(
+            created__range=(last_30_days_start, current_date)
+        )
 
-    number_of_orders_last_30_days = orders_last_30_days.count()
+        number_of_orders_last_30_days = orders_last_30_days.count()
 
-    # Calculate the total price and count of orders
-    total_price_last_30_days = sum(order.get_order_cost() for order in orders_last_30_days)
+        # Calculate the total price and count of orders
+        total_price_last_30_days = sum(order.get_order_cost() for order in orders_last_30_days)
 
-    if number_of_orders_last_30_days > 0:
-        average_price_last_30_days = total_price_last_30_days / number_of_orders_last_30_days
+        if number_of_orders_last_30_days > 0:
+            average_price_last_30_days = total_price_last_30_days / number_of_orders_last_30_days
+        else:
+            average_price_last_30_days ="Sin pedidos en los últimos 30 días"
+        
+        all_orders = Order.objects.all()
+        total_benefits = sum(order.get_order_cost() for order in all_orders)
+
+        preferred_shipping_last_30_days = orders_last_30_days.values('shipping_method').annotate(shipping_count=Count('shipping_method')).order_by('-shipping_count').first()
+
+        if preferred_shipping_last_30_days:
+            preferred_shipping_option = preferred_shipping_last_30_days['shipping_method']
+        else:
+            preferred_shipping_option = "Sin pedidos en los últimos 30 días"
+
+        most_popular_products = OrderItem.objects.filter(
+            order__created__range=(last_30_days_start, current_date)
+        ).values('product__id', 'product__name').annotate(total_quantity=Sum('quantity')).order_by('-total_quantity')[:5]
+
+        # Retrieve product details for the most popular products
+        popular_products = []
+        for popular_product in most_popular_products:
+            product_id = popular_product['product__id']
+            total_quantity = popular_product['total_quantity']
+            product = Product.objects.get(id=product_id)
+            popular_products.append({'product': product, 'total_quantity': total_quantity})
+
+
+
+        return render(request, "base/sales_report.html",{
+            "number_of_orders_last_30_days": number_of_orders_last_30_days,
+            "average_price_last_30_days": average_price_last_30_days,
+            "total_price_last_30_days": total_price_last_30_days,
+            "total_benefits": total_benefits,
+            "preferred_shipping_option": preferred_shipping_option,
+            "popular_products": popular_products,
+        })
     else:
-        average_price_last_30_days ="Sin pedidos en los últimos 30 días"
-    
-    all_orders = Order.objects.all()
-    total_benefits = sum(order.get_order_cost() for order in all_orders)
-
-    preferred_shipping_last_30_days = orders_last_30_days.values('shipping_method').annotate(shipping_count=Count('shipping_method')).order_by('-shipping_count').first()
-
-    if preferred_shipping_last_30_days:
-        preferred_shipping_option = preferred_shipping_last_30_days['shipping_method']
-    else:
-        preferred_shipping_option = "Sin pedidos en los últimos 30 días"
-
-    most_popular_products = OrderItem.objects.filter(
-        order__created__range=(last_30_days_start, current_date)
-    ).values('product__id', 'product__name').annotate(total_quantity=Sum('quantity')).order_by('-total_quantity')[:5]
-
-    # Retrieve product details for the most popular products
-    popular_products = []
-    for popular_product in most_popular_products:
-        product_id = popular_product['product__id']
-        total_quantity = popular_product['total_quantity']
-        product = Product.objects.get(id=product_id)
-        popular_products.append({'product': product, 'total_quantity': total_quantity})
-
-
-
-    return render(request, "base/sales_report.html",{
-        "number_of_orders_last_30_days": number_of_orders_last_30_days,
-        "average_price_last_30_days": average_price_last_30_days,
-        "total_price_last_30_days": total_price_last_30_days,
-        "total_benefits": total_benefits,
-        "preferred_shipping_option": preferred_shipping_option,
-        "popular_products": popular_products,
-    })
-
+        return redirect("/auth/login")        # Get the current date and time in the current time zone
+        
