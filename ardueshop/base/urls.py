52061b38ea6b4ea36d0c189cecaa33e6f68c8bc3
Francisco Benitez <frabenrui1@alum.us.es>
2023-12-08 18:44:26 +0100

    feat: Manage sales #58

--- ardueshop/base/urls.py
+++ ardueshop/base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path('order_not_found/', views.order_not_found, name='order_not_found')
+    path('order_not_found/', views.order_not_found, name='order_not_found'),
+    path('sales_report/', views.sales_report, name='sales_report')
 ]
