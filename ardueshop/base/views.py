e12f30e319a0a6555771b4306f283dff77c2d0ba
davidg43 <david.gavira.43@gmail.com>
2023-12-09 18:25:16 +0100

    Showcase in home view

--- ardueshop/base/views.py
+++ ardueshop/base/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from product.models import Category
+from product.models import Category, Product
 
 
 def home(request):
+    products = Product.objects.all().order_by("?")[:3]
     categories = Category.objects.all()
-    return render(request, "base/home.html", {"categories": categories})
+    return render(request, "base/home.html", {"categories": categories, "products": products})
 
 def about(request):
     return render(request, "base/about.html")
 def order_not_found(request):
     return render(request, "order/order_not_found.html")
+
+
 
